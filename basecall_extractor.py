19de042332a9378d7d17690e297e2a94159ca809
Russell Durrett <russdurrett@gmail.com>
2019-04-26 10:51:03 -0500

    in process

--- basecall_extractor.py
+++ basecall_extractor.py
@@ -29,33 +29,34 @@ samfile = pysam.AlignmentFile(bamfile_handle, "rb" )
 
 df = pd.DataFrame()
 
-for pileupcolumn in samfile.pileup(str(query_position[0]), int(query_position[1]), int(query_position[1])):
+for pileupcolumn in samfile.pileup(str(query_position[0]), int(query_position[1])-1, int(query_position[1])+1):
 	try: 
 		pileupcolumn.get_query_sequences()
 	except AssertionError: 
 		print('Skipping', pileupcolumn.reference_name, pileupcolumn.pos, 'because of pileupcolumn.get_query_sequences AssertionError problem')
 	else: 
-		bd = od()
-		bd['chr']=pileupcolumn.reference_name
-		bd['pos']=pileupcolumn.pos 
-		query_sequences = [g.upper() for g in pileupcolumn.get_query_sequences()]
-		query_base_counts = [query_sequences.count(b) for b in set(bases+list(set(query_sequences)))]
-		print('Base', bd['chr'], bd['pos'], '\t', '\t'.join([str(b)+":"+str(c) for b,c in zip(bases,query_base_counts)]))
-		for pileupread in pileupcolumn.pileups: 
-			if pileupread.query_position!=None:
-				rd = bd.copy()
-				pileupalignment = pileupread.alignment
-				pileupalignmenttags = od(pileupalignment.tags)
-				if 'CB' in pileupalignmenttags.keys():
-					rd['cell'] = pileupalignmenttags['CB'].split('-')[0]
-				else:
-					rd['cell'] = pileupalignmenttags['CR'].split('-')[0]
-				if 'UB' in pileupalignmenttags.keys():
-					rd['umi'] = pileupalignmenttags['UB'] 
-				else:
-					rd['umi'] = pileupalignmenttags['UR']
-				rd['base_call'] = pileupalignment.query_sequence[pileupread.query_position]
-				df=df.append(rd, ignore_index=True)
+		if pileupcolumn.pos==int(query_position[1]):
+			bd = od()
+			bd['chr']=pileupcolumn.reference_name
+			bd['pos']=pileupcolumn.pos 
+			query_sequences = [g.upper() for g in pileupcolumn.get_query_sequences()]
+			query_base_counts = [query_sequences.count(b) for b in set(bases+list(set(query_sequences)))]
+			print('Base', bd['chr'], bd['pos'], '\t', '\t'.join([str(b)+":"+str(c) for b,c in zip(bases,query_base_counts)]))
+			for pileupread in pileupcolumn.pileups: 
+				if pileupread.query_position!=None:
+					rd = bd.copy()
+					pileupalignment = pileupread.alignment
+					pileupalignmenttags = od(pileupalignment.tags)
+					if 'CB' in pileupalignmenttags.keys():
+						rd['cell'] = pileupalignmenttags['CB'].split('-')[0]
+					else:
+						rd['cell'] = pileupalignmenttags['CR'].split('-')[0]
+					if 'UB' in pileupalignmenttags.keys():
+						rd['umi'] = pileupalignmenttags['UB'] 
+					else:
+						rd['umi'] = pileupalignmenttags['UR']
+					rd['base_call'] = pileupalignment.query_sequence[pileupread.query_position]
+					df=df.append(rd, ignore_index=True)
 			# print(len(df), 'base calls written thus far')
 
 # df.to_csv('mtDNA_basecalls.final.tsv', sep='\t', index=False)
