803c80d1a8cacf0d7192044dc77a8dfe1f496fbc
Russell Durrett <russdurrett@gmail.com>
2019-04-25 22:06:16 -0500

    tweak

--- split_bam_by_cell.sh
+++ split_bam_by_cell.sh
@@ -7,12 +7,20 @@
 #cat cell_list.txt | while read CELL_BARCODE; do samtools view -F 1024 -h possorted_genome_bam.splitncigar.BQSR.bam |  awk -v tag="CB:Z:$CELL_BARCODE" '($0 ~ /^@/ || index($0,tag)>0)' > split_by_cell/${CELL_BARCODE}.sam ; done
 
 # MUCH faster: (awks the cell barcode identity per line and prints to respective file. loops thorugh bam only once)
+# still fucked up though... 
+# samtools view ../possorted_genome_bam.splitncigar.BQSR.bam | head | \
+# while read line; do
+#         cell=$(awk '{split($12,cbs,":"); print cbs[3]}' <<< $line);
+#   echo $line > $cell
+# done
+
+# faster and actually works! 
 samtools view possorted_genome_bam.splitncigar.BQSR.bam | \
 while read line; do
-        cell=$(awk '{split($12,cbs,":"); print cbs[3]}' <<< $line);
-  echo $line > split_by_cell/$cell
+  cell=$(awk '{ for(i=1;i<=NF;++i){ if ($i ~ /CB:Z:/) {cell=split($i,cbs,":"); print cbs[3] } } }' <<< $line);
+  echo $cell 
+  echo $line >> split_by_cell/$cell
 done
-
 
 
 
