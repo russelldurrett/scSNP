1a274a13cdebcc924cd8c902029bcf892a88312f
Russell Durrett <russdurrett@gmail.com>
2019-04-25 21:40:01 -0500

    lightyears faster

--- split_bam_by_cell.sh
+++ split_bam_by_cell.sh
@@ -3,6 +3,16 @@
 #samtools view -F 1024 possorted_genome_bam.splitncigar.BQSR.bam | cut -f 12- | tr "\t" "\n"  | grep  "^CB:Z:"  | cut -d ':' -f 3 | sort | uniq > cell_list.txt 
 # instead get filtered list of cell barcodes from cellranger out 
 
+# too slow: (loops through bam file over and over filtering out a specific cell)
+#cat cell_list.txt | while read CELL_BARCODE; do samtools view -F 1024 -h possorted_genome_bam.splitncigar.BQSR.bam |  awk -v tag="CB:Z:$CELL_BARCODE" '($0 ~ /^@/ || index($0,tag)>0)' > split_by_cell/${CELL_BARCODE}.sam ; done
 
-cat cell_list.txt | while read CELL_BARCODE; do samtools view -F 1024 -h possorted_genome_bam.splitncigar.BQSR.bam |  awk -v tag="CB:Z:$CELL_BARCODE" '($0 ~ /^@/ || index($0,tag)>0)' > split_by_cell/${CELL_BARCODE}.sam ; done
+# MUCH faster: (awks the cell barcode identity per line and prints to respective file. loops thorugh bam only once)
+samtools view possorted_genome_bam.splitncigar.BQSR.bam | \                                                                      [21:34:18]
+while read line; do
+        cell=$(awk '{split($12,cbs,":"); print cbs[3]}' <<< $line);
+  echo $line > split_by_cell/$cell
+done
+
+
+
 
